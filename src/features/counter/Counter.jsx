0a23c94c368fd83da6956aee38ed8336c0e64980
Saul Cortes <92839820+inscious@users.noreply.github.com>
2023-02-03 08:04:55 -0800

    updated css

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -13,40 +13,40 @@ const Counter = () => {
     const dispatch = useDispatch();
 
     return (
-        <div className="flex flex-col max-w-md items-center justify-center bg-stone-00 w-full bg-slate-00">
-            <span className="text-white text-4xl text">{count}</span>
+        <div className="flex flex-col max-w-xs lg:max-w-md items-center justify-center bg-stone-00 w-full bg-slate-00 duration-150">
+            <span className="text-slate-600 text-4xl text">{count}</span>
             <button
                 aria-label="Increment value"
                 onClick={() => dispatch(increment())}
-                className="text-black rounded-lg py-2 w-full bg-slate-500 hover:text-white duration-100 my-5 hover:bg-slate-400"
+                className="text-black rounded-lg py-5 lg:py-2 w-full bg-slate-200 hover:text-white duration-100 my-5 hover:bg-slate-400"
             >
                 Increment
             </button>
             <button
                 aria-label="Decrement value"
                 onClick={() => dispatch(decrement())}
-                className="text-black rounded-lg py-2 w-full bg-slate-500 hover:text-white duration-100 mb-5 hover:bg-slate-400"
+                className="text-black rounded-lg py-5 lg:py-2 w-full bg-slate-200 hover:text-white duration-100 mb-5 hover:bg-slate-400"
             >
                 Decrement
             </button>
             <button
                 aria-label="Decrement value"
                 onClick={() => dispatch(incrementBy10())}
-                className="text-black rounded-lg py-2 w-full bg-slate-500 hover:text-white duration-100 mb-5 hover:bg-slate-400"
+                className="text-black rounded-lg py-5 lg:py-2 w-full bg-slate-200 hover:text-white duration-100 mb-5 hover:bg-slate-400"
             >
                 Increment by 10
             </button>
             <button
                 aria-label="Decrement value"
                 onClick={() => dispatch(decrementBy10())}
-                className="text-black rounded-lg py-2 w-full bg-slate-500 hover:text-white duration-100 mb-5 hover:bg-slate-400"
+                className="text-black rounded-lg py-5 lg:py-2 w-full bg-slate-200 hover:text-white duration-100 mb-5 hover:bg-slate-400"
             >
                 Decrement by 10
             </button>
             <button
                 aria-label="Decrement value"
                 onClick={() => dispatch(resetCounter())}
-                className="text-black rounded-lg py-2 w-full bg-slate-500 hover:text-white duration-100 mb-5 hover:bg-slate-400"
+                className="text-black rounded-lg py-5 lg:py-2 w-full bg-slate-200 hover:text-white duration-100 mb-5 hover:bg-slate-400"
             >
                 Reset
             </button>
